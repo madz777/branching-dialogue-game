a902a47d3a1b51c735326bdfb9e2a39e142fe832
madz7812 <maddyc322@gmail.com>
2021-12-03 00:05:04 -0500

    the game is running really basically!

--- src/src/Game.java
+++ src/src/Game.java
@@ -1,72 +1,129 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Game {
-    private JFrame window;
-    private Container con;
-    private JPanel narText, cont, choice;
-    private JLabel textLabel, cLabel;
-    private JButton contButton;
-    Font textFont = new Font("Arial", Font.PLAIN, 16);
-
-    static Path a1 = new Path(0, 0, "a1");
-    static Path b1 = new Path(3, 3, "b1");
-    public static Path pathCurrent = b1;
-
-    private boolean nar = true;
-
+    //Main
     public static void main(String[] args) {
-        //Path a1 = new Path(0, 0, "a1");
         pathCurrent.setDialogue();
         new Game();
     }
 
+    private Container con;
+    private JFrame window;
+    private JPanel narText, cont, choices, inv;
+    private JButton contButton, invButton, choiceButtonA, choiceButtonB, choiceButtonE;
+    private JTextArea text;
+    public static boolean nar = true;
+
+    private GlobalListener listener = new GlobalListener();
+
+    Font textFont = new Font("Arial", Font.PLAIN, 16);
+
+    public static Path pathCurrent = new Path(3, 3, "b1", false);
+
     public Game() {
-
-
         //Make the Window
         window = new JFrame();
         window.setSize(800, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.darkGray);
         window.setLayout(null);
-
         con = window.getContentPane();
 
         //Narration Text
         narText = new JPanel();
         narText.setBounds(100,100,600,150);
         narText.setBackground(Color.darkGray);
+        text = new JTextArea(pathCurrent.dialogueCurrent());
+        text.setBounds(100, 100, 600, 150);
+        text.setBackground(Color.darkGray);
+        text.setForeground(Color.white);
+        text.setFont(textFont);
+        text.setLineWrap(true);
+        narText.add(text);
 
-        textLabel = new JLabel(pathCurrent.dialogueCurrent(), SwingConstants.LEFT);
-        textLabel.setForeground(Color.white);
-        textLabel.setFont(textFont);
+        //Inventory
+        inv = new JPanel();
+        inv.setBounds(10, 10, 100, 40);
+        inv.setBackground(Color.darkGray);
+        invButton = new JButton("Inventory");
+        invButton.setForeground(Color.white);
+        invButton.setBackground(Color.black);
+        invButton.setPreferredSize(new Dimension(100, 30));
+        invButton.setFocusable(false);
+        inv.add(invButton);
 
-        //Buttons
         if (nar) {
+            //Continue Button
             cont = new JPanel();
-            cont.setBounds(10, 520, 760, 40);
+            cont.setBounds(12, 490, 760, 40);
             cont.setBackground(Color.darkGray);
             contButton = new JButton("Continue...");
             contButton.setForeground(Color.white);
             contButton.setBackground(Color.black);
             contButton.setPreferredSize(new Dimension(760, 30));
             contButton.setFocusable(false);
+            contButton.addActionListener(listener);
             cont.add(contButton);
-        } else {
-            //Choice Buttons
-
         }
-
-        narText.add(textLabel);
+        con.add(inv);
         con.add(narText);
         con.add(cont);
-
         window.setVisible(true);
     }
 
+    public void setChoiceMode() {
+        cont.setVisible(false);
+
+        choices = new JPanel();
+        choices.setBounds(12, 450, 760, 120);
+        choices.setBackground(Color.darkGray);
+
+        choiceButtonE = new JButton(pathCurrent.choiceE());
+        choiceButtonE.setForeground(Color.white);
+        choiceButtonE.setBackground(Color.black);
+        choiceButtonE.setPreferredSize(new Dimension(760, 30));
+        choiceButtonE.setFocusable(false);
+        choiceButtonE.addActionListener(listener);
+
+        choiceButtonA = new JButton(pathCurrent.choice1());
+        choiceButtonA.setForeground(Color.white);
+        choiceButtonA.setBackground(Color.black);
+        choiceButtonA.setPreferredSize(new Dimension(760, 30));
+        choiceButtonA.setFocusable(false);
+        choiceButtonA.addActionListener(listener);
+
+        choiceButtonB = new JButton(pathCurrent.choice2());
+        choiceButtonB.setForeground(Color.white);
+        choiceButtonB.setBackground(Color.black);
+        choiceButtonB.setPreferredSize(new Dimension(760, 30));
+        choiceButtonB.setFocusable(false);
+        choiceButtonB.addActionListener(listener);
 
 
+        choices.add(choiceButtonB);
+        choices.add(choiceButtonA);
+        choices.add(choiceButtonE);
 
+        //choiceButtonE.setVisible(pathCurrent.hasExtra());
+
+        con.add(choices);
+    }
+
+    public class GlobalListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JButton buttonName = (JButton) e.getSource();
+            if (buttonName.equals(contButton)) {
+                pathCurrent.setDialogue();
+                text.setText(pathCurrent.dialogueCurrent());
+                if (!nar) setChoiceMode();
+            } else if (buttonName.equals("Button 2")) {
+
+            } else {
+
+            }
+        }
+    }
 }
 
--- src/src/Path.java
+++ src/src/Path.java
@@ -1,17 +1,22 @@
 public class Path {
-    private int textLength;
+    private int indexLength;
     private int numChoices;
     private String pathIndex;
     private String dialogue;
     private int dialogueIndex;
+    private String c1;
+    private String c2;
+    private String cE = "";
+    private boolean extra = false;
 
-    public Path(int t, int c, String n) {
-        textLength = t;
+
+    public Path(int t, int c, String n, boolean e) {
+        indexLength = t;
         numChoices = c;
         pathIndex = n;
+        extra = e;
 
         dialogueIndex = 0;
-
     }
 
     private int getDialogueIndex() {
@@ -26,28 +31,50 @@ public class Path {
         return pathIndex;
     }
 
-    public String dialogueCurrent() {
-        return dialogue;
+    public boolean hasExtra() {
+        return extra;
     }
 
     public void setDialogue() {
         String d = "";
-        switch(Game.pathCurrent.getPathIndex()) {
+        switch(pathIndex) {
             case "a1":
                 d = "";
                 break;
             case "b1":
-                switch (Game.pathCurrent.getDialogueIndex()) {
-                    case 0:
-                        System.out.print("hi");
-                        d = "You are dead. ";
-                        break;
-                    case 1:
-
+                switch (dialogueIndex) {
+                    case 0 -> d = "'You are dead. '";
+                    case 1 -> d = "You hear a voice, but see nothing. ";
+                    case 2 -> {
+                        d = "What do you do?";
+                        c1 = "Who are you?";
+                        c2 = "Where am I?";
+                    }
                 }
         }
         dialogue = d;
+        if (!(dialogueIndex > indexLength)) {
+            dialogueIndex++;
+            if (dialogueIndex == indexLength) {
+                Game.nar = false;
+            }
+        }
+
     }
 
+    public String dialogueCurrent() {
+        return dialogue;
+    }
 
+    public String choice1() {
+        return c1;
+    }
+
+    public String choice2() {
+        return c2;
+    }
+
+    public String choiceE() {
+        return cE;
+    }
 }
